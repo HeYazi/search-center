d8e0d19259115f64708423dace68af0653bd8888
heyazi <hegd0523@qq.com>
2023-07-15 15:44:55 +0800

    Changes

---
+++ src/test/java/com/hyz/searchcenter/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyz.searchcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
